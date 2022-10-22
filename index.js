c05c4bd7f8a01d135e3352bcecb51419fc8106a5
adil-khursheed <adilkhursheed60@gmail.com>
2022-10-22 09:15:00 +0530

    more logic added

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const questions = document.querySelectorAll(".question-answer-section");
 const text = document.querySelector(".ques-text");
 
+
 questions.forEach((question) => {
     question.addEventListener("click", () => {
-        question.classList.toggle("active");
-        // text.style.fontWeight = "700";
+        // question.classList.toggle("active");
+        if (question.classList.contains("active")) {
+            question.classList.remove("active");
+        } else {
+            const questionActive = document.querySelectorAll(".active");
+            questionActive.forEach((activeQuestion) => {
+                activeQuestion.classList.remove("active");
+            })
+            question.classList.add("active");
+            
+        }
+        
     })
 })
